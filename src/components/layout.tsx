d123ccf5c600daf1a8b45032bc4f2049615bbf17
sepans <sepans@sepans.com>
2020-05-25 19:42:05 -0500

    some more posts

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,79 +6,40 @@
  */
 
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Sidebar from "./sidebar"
 import "./layout.css"
 import styled from "styled-components"
+
 
 export interface LayoutProps {
   children: React.ReactNode
 }
 
 const Layout: React.FC<LayoutProps> = (props) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-  const postData = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      allMdx(filter: {}) {
-        nodes {
-          id
-          excerpt(pruneLength: 40)
-          rawBody
-          wordCount {
-            words
-          }
-          frontmatter {
-            title
-            slug
-          }
-          fileAbsolutePath
-          timeToRead
-          html
-        }
-      }
-    }
-  `)
 
-const posts = postData.allMdx.nodes.map((post: any) => {
-  console.log(post, post.excerpt)
-  return (
-      <>
-        <a href="/content/">{post.frontmatter.title}</a>
-        <div key={post.frontmatter.slug}>{post.excerpt}</div>
-      </>
-    )
-  })
-  console.log(postData, posts)
-
+ 
   return (
     <>
       <Header />
       <Container>
         <MainArea>{props.children}</MainArea>
-        <Sidebar>{posts}</Sidebar>
+        <Sidebar/>
       </Container>
     </>
   )
 }
 
-
 const Container = styled.div`
   display: flex;
   max-width: 960;
-  padding: 1rem 1.0875rem 1.45rem;
+  padding: 20px;
 `
 const MainArea = styled.div`
   flex: 1;
+  padding: 20px;
 `
-const Sidebar = styled.div``
+
 
 export default Layout
