e5918db475755463544a9dce3008f871b86ff3c7
raschin <raschin@gmail.com>
2022-03-06 12:12:43 -0500

    add styled-components plugin to get server-side css and avoid style flashing

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,9 @@ module.exports = {
       },
     },
     "gatsby-plugin-offline",
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-typescript",
     {
@@ -57,9 +60,7 @@ module.exports = {
       options: {
         trackingId: "UA-11178641-2",
       },
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    },
+    "gatsby-plugin-styled-components"
   ],
 }
